43730dcc1e53d6b2c8feae0cd6670463c64eb332
Eddie Jaoude <eddie@jaoudestudios.com>
2020-05-23 19:35:43 +0100

    feat(quiz): updated header layout on question to improve usability, inc question number

--- hosting/src/app/games/quiz/question/question.component.ts
+++ hosting/src/app/games/quiz/question/question.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnChanges } from '@angular/core';
 
-import { map, tap } from 'rxjs/operators';
+import { Choice, Question, QuestionSummary, Quiz, Round } from '../quiz.model';
 
-import { Choice, Question, Quiz, Round } from '../quiz.model';
+import { Subscription } from 'rxjs';
+
 import { AnswerService } from '../answer.service';
+import { delay, map, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'game-question',
@@ -14,7 +16,10 @@ import { AnswerService } from '../answer.service';
 export class QuestionComponent implements OnChanges {
 
     public saved = false;
+    public saving = false;
     public response: Array<string> = [];
+
+    private subscription: Subscription;
 
     @Input()
     public quiz: Quiz;
@@ -29,12 +34,22 @@ export class QuestionComponent implements OnChanges {
         private answerService: AnswerService,
     ) { }
 
+    get progress(): string {
+        const list = (<Array<QuestionSummary>> this.round.questionList);
+        const current = list.findIndex((question) => question.uid === this.question.uid) + 1;
+        return `${current} / ${list.length}`;
+    }
+
     public ngOnChanges(): void {
         if (!this.question) {
             return;
         }
 
-        this.answerService
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+
+        this.subscription = this.answerService
             .findOne(this.quiz.uid, this.round.uid, this.question.uid)
             .pipe(
                 tap((answer) => this.saved = !!answer)
@@ -54,15 +69,17 @@ export class QuestionComponent implements OnChanges {
         if (idx >= 0) {
             this.response.splice(idx, 1);
         } else {
-            this.response.push(choice.uid);
+            this.response = [ ...this.response, choice.uid ];
         }
     }
 
     public saveAnswer(): void {
-        this.answerService
-            .create(this.quiz.uid, this.round.uid, this.question.uid, this.response)
+        this.answerService.create(this.quiz.uid, this.round.uid, this.question.uid, this.response)
             .pipe(
+                map(() => this.saving = true),
+                delay(800),
                 map(() => this.saved = true),
+                map(() => this.saving = false),
             )
             .subscribe();
     }
